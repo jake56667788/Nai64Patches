baebf431d001067872172efc16e69fa59a9b7844
jake56667788 <247085573+jake56667788@users.noreply.github.com>
2026-09-17 19:33:16 -0400

    Update build.gradle.kts

--- patches/build.gradle.kts
+++ patches/build.gradle.kts
@@ -1,27 +1,9 @@
-plugins {
-    kotlin("jvm")
-}
-
 group = "com.github.jake56667788"
 version = "1.0.0"
 
-repositories {
-    mavenCentral()
-    google()
-    maven("https://jitpack.io")
-
-    maven("https://maven.pkg.github.com/MorpheApp/registry") {
-        credentials {
-            username = System.getenv("GITHUB_ACTOR") ?: "token"
-            password = System.getenv("GITHUB_TOKEN") ?: ""
-        }
-    }
-}
-
-tasks.withType<Jar>().configureEach {
-    manifest {
-        attributes(
-            "Name" to project.name
-        )
+patches {
+    about {
+        name = "Nai64 Patches"
+        description = "Custom patches for Nai64"
     }
 }
