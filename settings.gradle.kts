86d6ac1def61ef3db00dfc8b91859fd062c69bf9
jake56667788 <247085573+jake56667788@users.noreply.github.com>
2026-09-17 19:03:19 -0400

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,21 +1,13 @@
-rootProject.name = "Nai64Patches"
-
 pluginManagement {
     repositories {
         gradlePluginPortal()
         google()
-        maven {
-            name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/MorpheApp/registry")
+        mavenCentral()
+        maven("https://maven.pkg.github.com/MorpheApp/registry") {
             credentials {
-                username = providers.gradleProperty("gpr.user").orNull ?: System.getenv("GITHUB_ACTOR")
-                password = providers.gradleProperty("gpr.key").orNull ?: System.getenv("GITHUB_TOKEN")
+                username = System.getenv("GITHUB_ACTOR") ?: "token"
+                password = System.getenv("GITHUB_TOKEN") ?: ""
             }
         }
-        maven { url = uri("https://jitpack.io") }
     }
-}
-
-plugins {
-    id("app.morphe.patches") version "1.3.4"
 }
