24ceef98aade9a62fab475589c888accfb69245a
jake56667788 <247085573+jake56667788@users.noreply.github.com>
2026-09-17 19:10:37 -0400

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,3 +11,20 @@ pluginManagement {
         }
     }
 }
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+        maven("https://maven.pkg.github.com/MorpheApp/registry") {
+            credentials {
+                username = System.getenv("GITHUB_ACTOR") ?: "token"
+                password = System.getenv("GITHUB_TOKEN") ?: ""
+            }
+        }
+    }
+}
+
+rootProject.name = "Nai64Patches"
